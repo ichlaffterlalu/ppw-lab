c15ada93204b2a62948cb5df96358acdc05fc9e6
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-02 10:59:50 +0700

    Implemented calculate_age function

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
+# Calculates age.
+def calculate_age(birth_year):
+    return 2017 - birth_year
+
 # Enter your name here
-mhs_name = 'Ichlasul Affan' # TODO Implement this
+mhs_name = 'Ichlasul Affan'
+age = calculate_age(1999)
 
 # Create your views here.
 def index(request):
-    response = {'name': mhs_name}
+    response = {'name': mhs_name, 'age': age}
     return render(request, 'index.html', response)
-
-# TODO Implement this to complete last checklist
-def calculate_age(birth_year):
-    pass
