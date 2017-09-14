2c45e3d965aa1b3a4d0c5af1762422abccdd7e73
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-14 11:29:53 +0700

    Get back lab_1 and lab_2 implementations while keeping upstream updates

--- lab_2/urls.py
+++ lab_2/urls.py
@@ -4,9 +4,5 @@ from .views import index
 #url for app, add your URL Configuration
 
 urlpatterns = [
-<<<<<<< HEAD
 	url(r'^$', index, name='index'),
-=======
-#TODO Implement this
->>>>>>> 6146a3717e79a7c79f6b6ffa0e210a31f03c9d8a
 ]
--- lab_2/views.py
+++ lab_2/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from lab_1.views import mhs_name, birth_date
 
-<<<<<<< HEAD
 #Content paragraph for landing page
 landing_page_content = "I am an ambitious-looked guy but I am not really that ambitious."
-=======
-#TODO Implement
-#Create a content paragraph for your landing page:
-landing_page_content = ''
->>>>>>> 6146a3717e79a7c79f6b6ffa0e210a31f03c9d8a
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
