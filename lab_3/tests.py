713b0904ec1a827a643b81cf93c7459a9baf1350
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-14 14:42:53 +0700

    Created add_activity method in views in lab_3

--- lab_3/tests.py
+++ lab_3/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import resolve
 from .views import index
+from .models import Diary
+from django.utils import timezone
 
 # Create your tests here.
 class Lab3Test(TestCase):
@@ -22,3 +24,12 @@ class Lab3Test(TestCase):
 		#Retrieving all available activity
 		counting_all_available_activity = Diary.objects.all().count()
 		self.assertEqual(counting_all_available_activity,1)
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/lab-3/add_activity/', data={'date': '2017-10-12T14:14', 'activity' : 'Maen Dota Kayaknya Enak'})
+		counting_all_available_activity = Diary.objects.all().count()
+		self.assertEqual(counting_all_available_activity, 1)
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/lab-3/')
+		new_response = self.client.get('/lab-3/')
+		html_response = new_response.content.decode('utf8')
+		self.assertIn('Maen Dota Kayaknya Enak', html_response)
--- lab_3/urls.py
+++ lab_3/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, add_activity
 #url for app
 urlpatterns = [
 	url(r'^$', index, name='index'),
+	url(r'add_activity/$', add_activity, name='add_activity')
 ]
