fa00008498af031c06c5d67cc5be8f5882598938
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-14 13:04:13 +0700

    Minor changes to adjust with new template

--- lab_2/views.py
+++ lab_2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from lab_1.views import mhs_name, birth_date
 
 #Content paragraph for landing page
-landing_page_content = "I am an ambitious-looked guy but I am not really that ambitious."
+landing_page_content = "I am an ambitious-looked guy but I am not really that ambitious"
 
 def index(request):
     response = {'name': mhs_name, 'content': landing_page_content}
