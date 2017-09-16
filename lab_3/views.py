305fbf06cf98bf148e303de691edc5982089db66
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-17 01:57:42 +0700

    Added error handling (regex) on diary date input

--- lab_3/views.py
+++ lab_3/views.py
@@ -3,6 +3,7 @@ from .models import Diary
 from datetime import datetime
 import pytz
 import json
+import re
 # Create your views here.
 diary_dict = {}
 def index(request):
@@ -11,6 +12,11 @@ def index(request):
 
 def add_activity(request):
     if request.method == 'POST':
+        # Validating input using regex
+        pattern = re.compile('\d\d\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):[0-5][0-9]')
+        if not pattern.match(request.POST['date']): return redirect('/lab-3/')
+        
+        # If input is valid
         date = datetime.strptime(request.POST['date'],'%Y-%m-%dT%H:%M')
         Diary.objects.create(date=date.replace(tzinfo=pytz.UTC),activity=request.POST['activity'])
         return redirect('/lab-3/')
