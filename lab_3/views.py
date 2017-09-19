30f9c854bf3148003172c18978c91e4cc49b1417
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-19 12:21:51 +0700

    Minor improvement on error handling

--- lab_3/views.py
+++ lab_3/views.py
@@ -18,7 +18,7 @@ def add_activity(request):
         
         try:
             date = datetime.strptime(request.POST['date'],'%Y-%m-%dT%H:%M')
-            Diary.objects.create(date=date.replace(tzinfo=pytz.UTC),activity=request.POST['activity'])
+            Diary.objects.create(date=date.replace(tzinfo=pytz.UTC),activity=request.POST['activity'].strip())
             return redirect('/lab-3/')
         except ValueError:
             messages.add_message(request, messages.ERROR, 'ERROR: Date should be from 0001-01-01T00:00 to 9999-31-12T23:59.')
