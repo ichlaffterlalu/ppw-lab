b5aeb57ed789cb90241fbd6a337e872b3209fc25
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-20 11:25:00 +0700

    Implemented message form on lab_4

--- lab_4/tests.py
+++ lab_4/tests.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import resolve
 from django.http import HttpRequest
-from .views import index, about_me, landing_page_content
+from .views import index, about_me, landing_page_content, index, message_post
+from lab_1.views import mhs_name
+from .models import Message
+from .forms import Message_Form
 # Create your tests here.
 
 class Lab4UnitTest(TestCase):
@@ -27,4 +30,39 @@ class Lab4UnitTest(TestCase):
 
 		#Chceking whether all About Me Item is rendered
 		for item in about_me:
-			self.assertIn(item,html_response)      
+			self.assertIn(item,html_response)
+
+	def test_model_can_create_new_message(self):
+		#Creating a new activity
+		new_activity = Message.objects.create(name=mhs_name,email='test@gmail.com',message='This is a test')
+
+		#Retrieving all available activity
+		counting_all_available_message= Message.objects.all().count()
+		self.assertEqual(counting_all_available_message,1)
+
+	def test_form_message_input_has_placeholder_and_css_classes(self):
+		form = Message_Form()
+		self.assertIn('class="form-control"', form.as_p())
+		self.assertIn('<label for="id_name">Nama:</label>', form.as_p())
+		self.assertIn('<label for="id_email">Email:</label>', form.as_p())
+		self.assertIn('<label for="id_message">Message:</label>', form.as_p())
+
+	def test_form_validation_for_blank_items(self):
+		form = Message_Form(data={'name': '', 'email': '', 'message': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(
+			form.errors['message'],
+			["This field is required."]
+		)
+	def test_lab4_post_fail(self):
+		response = Client().post('/lab-4/add_message', {'name': 'Anonymous', 'email': 'A', 'message': ''})
+		self.assertEqual(response.status_code, 302)
+
+	def test_lab4_post_success_and_render_the_result(self):
+		anonymous = 'Anonymous'
+		message = 'HaiHai'
+		response = Client().post('/lab-4/add_message', {'name': '', 'email': '', 'message': message})
+		self.assertEqual(response.status_code, 200)
+		html_response = response.content.decode('utf8')
+		self.assertIn(anonymous,html_response)
+		self.assertIn(message,html_response)
--- lab_4/views.py
+++ lab_4/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from lab_2.views import landing_page_content
+from django.http import HttpResponseRedirect
+from .forms import Message_Form
+from .models import Message
+
 
 # Create your views here.
 response = {'author': ""} #TODO Implement yourname
@@ -10,3 +14,19 @@ def index(request):
 	#TODO Implement, isilah dengan 6 kata yang mendeskripsikan anda
 	response['about_me'] = about_me
 	return render(request, html, response)
+	response['message_form'] = Message_Form
+	return render(request, html, response)
+
+def message_post(request):
+    form = Message_Form(request.POST or None)
+    if(request.method == 'POST' and form.is_valid()):
+        response['name'] = request.POST['name'] if request.POST['name'] != "" else "Anonymous"
+        response['email'] = request.POST['email'] if request.POST['email'] != "" else "Anonymous"
+        response['message'] = request.POST['message']
+        message = Message(name=response['name'], email=response['email'],
+                          message=response['message'])
+        message.save()
+        html ='lab_4/form_result.html'
+        return render(request, html, response)
+    else:        
+        return HttpResponseRedirect('/lab-4/')
