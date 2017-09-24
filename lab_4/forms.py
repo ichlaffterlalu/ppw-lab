808cdda6cba43a3f27c868e4dbc29f2fc01406a0
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-09-24 13:14:33 +0700

    Added client side input validation and custom required attributes

--- lab_4/forms.py
+++ lab_4/forms.py
@@ -2,13 +2,22 @@ from django import forms
 
 class Message_Form(forms.Form):
     error_messages = {
-        'required': 'Tolong isi input ini',
-        'invalid': 'Isi input dengan email',
+        'required': 'I am sad if you are not filling this field :(',
+        'invalid': 'Well... I think you have put in something wrong :(',
     }
-    attrs = {
-        'class': 'form-control'
+    attrs_name = {
+        'class': 'form-control',
+        'placeholder': 'Dennis Dien Pardede'
+    }
+    attrs_email = {
+        'class': 'form-control',
+        'placeholder': 'example@example.com'
+    }
+    attrs_message = {
+        'class': 'form-control',
+        'placeholder': 'This is a message for you.'
     }
 
-    name = forms.CharField(label='Name', required=False, max_length=27, empty_value='Anonymous', widget=forms.TextInput(attrs=attrs))
-    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs=attrs))
-    message = forms.CharField(widget=forms.Textarea(attrs=attrs), required=True)
+    name = forms.CharField(label='Name', required=True, max_length=27, empty_value='Anonymous', widget=forms.TextInput(attrs=attrs_name))
+    email = forms.EmailField(required=False, widget=forms.EmailInput(attrs=attrs_email))
+    message = forms.CharField(widget=forms.Textarea(attrs=attrs_message), required=True)
