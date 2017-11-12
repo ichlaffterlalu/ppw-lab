b79d6aeca4cdc72320cfd74c61a3b406034697c6
Ichlasul Affan <ichlaffterlalu@gmail.com>
2017-11-12 22:31:58 +0700

    Implemented Fasilkom UI API on lab_7

--- lab_7/admin.py
+++ lab_7/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Friend
 
 # Register your models here.
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ('npm','friend_name','added_at')
+    list_display_links = ('npm','friend_name')
+    search_fields = ('npm','friend_name')
+    list_per_page = 50
+
+admin.site.register(Friend,FriendAdmin)
